70e8867166fa4391a7dec00902982f99c5a088d9
valek90@bk.ru <valek90@bk.ru>
2023-02-15 17:03:21 +0300

    add Homework)

---
+++ Homework)/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("enter a digit");
+string str = Console.ReadLine();
+char[] arr = str.ToCharArray();
+if(arr.Length > 2)
+{
+    Console.WriteLine(arr[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
